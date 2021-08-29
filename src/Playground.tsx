7f4650bc1762f7b61d04fda267553816f43f8284
EDUARDO FELIPE SANTOS PINHEIRO <eduardo.pinheiro4@fatec.sp.gov.br>
2021-08-29 18:13:27 -0300

    Save Progress

--- src/Playground.tsx
+++ src/Playground.tsx
@@ -1,38 +1,17 @@
-import React, { ChangeEventHandler, useCallback, useMemo } from 'react';
+import { ChangeEventHandler, useCallback, useState } from 'react';
 import { Context } from './SimplexStateManager';
 
 const context = new Context();
-const TITLE = 'TITLE';
 
 function Playground() {
-  return (
-    <div>
-      <SetValue />
-      <LogValue />
-    </div>
-  );
-}
-
-function LogValue() {
-  const onLogValue = useCallback(() => {
-    const value = context.getState(TITLE);
-    console.log(value);
-  }, []);
-
-  return <div onClick={onLogValue}>Logar valor</div>;
-}
-
-function SetValue() {
-  const setState = useCallback((text: string) => {
-    context.setState(TITLE, text);
-  }, []);
+  const [title, setTitle] = useState<string>();
 
   const onChange: ChangeEventHandler<HTMLInputElement> = useCallback((event) => {
     const text = event?.target?.value;
-    setState(text);
+    setTitle(text);
   }, []);
 
-  return <input onChange={onChange} />;
+  return <input onChange={onChange} value={title} />;
 }
 
 export default Playground;
--- src/SimplexStateManager/Context/index.ts
+++ src/SimplexStateManager/Context/index.ts
@@ -7,6 +7,13 @@ export default class {
     this.createStore();
   }
 
+  newState<ValueType>(key: string) {
+    const dispatch = (value: ValueType) => this.setState<ValueType>(key, value);
+    const getState = () => this.getState<ValueType>(key);
+    const useState = () => [getState(), dispatch] as [ValueType, (value: ValueType) => void];
+    return { dispatch, getState, useState };
+  }
+
   private createStore() {
     OneAboveAll.setState(this.storeKey, { });
   }
@@ -15,12 +22,12 @@ export default class {
     return OneAboveAll.getState(this.storeKey) as Record<string, unknown>;
   }
 
-  getState<ValueType>(key: string) {
+  private getState<ValueType>(key: string) {
     const store = this.getStore();
     return store[key] as ValueType;
   }
 
-  setState<ValueType>(key: string, value: ValueType) {
+  private setState<ValueType>(key: string, value: ValueType) {
     const store = this.getStore();
     const newValue = value;
     store[key] = newValue;
